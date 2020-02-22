73579ff68e1a7512a2710ffdbb613a78f9536bac
MantaYi <318797061@qq.com>
2020-02-22 14:29:09 +0800

    项目初始化

---
+++ client/src/component/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-col :span="24">
+      <div class="header">
+        <img src alt />
+      </div>
+    </el-col>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.header {
+  width: 100%;
+  height: 100px;
+  background-color: #003472;
+}
+</style>
