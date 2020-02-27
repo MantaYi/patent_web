9a9fe7b04aa626cdf44a548670ce0593d6529a0a
MantaYi <318797061@qq.com>
2020-02-27 16:54:13 +0800

    文件新建与部分初始化

---
+++ client/src/views/userViews/space/MyFile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="myFile">
+    <p>我是文件</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyFile"
+};
+</script>
+<style lang="less" scoped>
+.myFile {
+  width: 1000px;
+  height: 500px;
+  margin: 0 auto;
+  margin-top: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+}
+</style>
---
+++ client/src/views/userViews/space/MyPatent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="myPatent">
+    <p>我是专利</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyPatent"
+};
+</script>
+<style lang="less" scoped>
+.myPatent {
+  width: 1000px;
+  height: 500px;
+  margin: 0 auto;
+  margin-top: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+}
+</style>
