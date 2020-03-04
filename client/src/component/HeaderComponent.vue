f3bcd98c851257de4f1edae895e050158bd25f6f
MantaYi <318797061@qq.com>
2020-03-04 09:49:59 +0800

    导航栏优化

--- client/src/component/HeaderComponent.vue
+++ client/src/component/HeaderComponent.vue
@@ -1,50 +1,25 @@
 <template>
   <div class="header">
-    <img src="../assets/logo.png" alt="patent for you" height="50px" class="left" />
+    <img src="../assets/logo.png" alt="patent for you" height="50px" />
     <el-menu
       :default-active="activeIndex2"
-      class="el-menu-demo center"
+      class="el-menu-demo header-nav"
       mode="horizontal"
       @select="handleSelect"
+      background-color="#003472"
       text-color="#fff"
       active-text-color="#ffd04b"
+      router
     >
-      <el-menu-item index="1">
-        <router-link to="/">主页</router-link>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <router-link to="/news">新闻</router-link>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <router-link to="/knowledge">知识</router-link>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <router-link to="/law">法律</router-link>
-      </el-menu-item>
-      <el-menu-item index="5">
-        <router-link to="/file">文件</router-link>
-      </el-menu-item>
-      <el-menu-item index="6">
-        <router-link to="/patent">专利</router-link>
-      </el-menu-item>
-      <el-menu-item index="7">
-        <router-link to="/manage">管理</router-link>
-      </el-menu-item>
-    </el-menu>
-    <el-menu
-      :default-active="activeIndex2"
-      class="el-menu-demo right"
-      mode="horizontal"
-      @select="handleSelect"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
-      <el-menu-item index="1">
-        <router-link to="/login">登录/注册</router-link>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <router-link to="/space">我的空间</router-link>
-      </el-menu-item>
+      <el-menu-item index="/">主页</el-menu-item>
+      <el-menu-item index="/news">新闻</el-menu-item>
+      <el-menu-item index="/knowledge">知识</el-menu-item>
+      <el-menu-item index="/law">法律</el-menu-item>
+      <el-menu-item index="/file">文件</el-menu-item>
+      <el-menu-item index="/patent">专利</el-menu-item>
+      <el-menu-item index="/manage">管理</el-menu-item>
+      <el-menu-item index="/login">登录/注册</el-menu-item>
+      <el-menu-item index="/space">我的空间</el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -57,26 +32,20 @@
   background-color: #003472;
   display: flex;
   justify-content: space-between;
+  overflow: hidden;
   img {
     margin-top: 5px;
     margin-left: 100px;
   }
-  .el-menu {
-    background-color: #003472;
-    color: #fff;
-  }
-  .left {
-    float: left;
-  }
-  .center {
-    float: left;
-    width: 800px;
-    margin-left: 100px;
-  }
-  .right {
-    float: right;
-    width: 200px;
-    margin-right: 100px;
+  .header-nav {
+    width: 1200px;
+    li {
+      margin: 0 10px;
+      text-align: center;
+      &:nth-child(7) {
+        margin-right: 100px;
+      }
+    }
   }
 }
 </style>
--- client/src/views/manageViews/ManageView.vue
+++ client/src/views/manageViews/ManageView.vue
@@ -7,50 +7,51 @@
           class="el-menu-vertical-demo"
           @open="handleOpen"
           @close="handleClose"
-          background-color="#545c64"
+          background-color="#003c86"
           text-color="#fff"
           active-text-color="#ffd04b"
+          router
         >
-          <el-menu-item index="1">
-            <i class="el-icon-menu"></i>
+          <el-menu-item index="/manage/file">
+            <i class="el-icon-folder-opened"></i>
             <span slot="title">文件管理</span>
           </el-menu-item>
           <el-submenu index="2">
             <template slot="title">
-              <i class="el-icon-location"></i>
+              <i class="el-icon-reading"></i>
               <span>专利知识</span>
             </template>
             <el-menu-item-group>
-              <el-menu-item index="2-1">专利知识管理</el-menu-item>
-              <el-menu-item index="2-2">专利知识增加</el-menu-item>
+              <el-menu-item index="/manage/knowledge">专利知识管理</el-menu-item>
+              <el-menu-item index="/manage/knowledgeAdd">专利知识增加</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
           <el-submenu index="3">
             <template slot="title">
-              <i class="el-icon-location"></i>
+              <i class="el-icon-coordinate"></i>
               <span>法律法规</span>
             </template>
             <el-menu-item-group>
-              <el-menu-item index="3-1">法律法规管理</el-menu-item>
-              <el-menu-item index="3-2">法律法规增加</el-menu-item>
+              <el-menu-item index="/manage/law">法律法规管理</el-menu-item>
+              <el-menu-item index="/manage/lawAdd">法律法规增加</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
           <el-submenu index="4">
             <template slot="title">
-              <i class="el-icon-location"></i>
+              <i class="el-icon-news"></i>
               <span>新闻</span>
             </template>
             <el-menu-item-group>
-              <el-menu-item index="4-1">新闻管理</el-menu-item>
-              <el-menu-item index="4-2">新闻增加</el-menu-item>
+              <el-menu-item index="/manage/news">新闻管理</el-menu-item>
+              <el-menu-item index="/manage/newsAdd">新闻增加</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
-          <el-menu-item index="5">
-            <i class="el-icon-menu"></i>
+          <el-menu-item index="/manage/patent">
+            <i class="el-icon-tickets"></i>
             <span slot="title">专利管理</span>
           </el-menu-item>
-          <el-menu-item index="6">
-            <i class="el-icon-menu"></i>
+          <el-menu-item index="/manage/user">
+            <i class="el-icon-user"></i>
             <span slot="title">用户管理</span>
           </el-menu-item>
         </el-menu>
@@ -69,8 +70,10 @@
   height: 600px;
   background-color: #fff;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-  margin: 0 auto;
+  margin: 20px auto;
   .el-header {
+    width: 1200px;
+    border-radius: 4px;
     background-color: #003c86;
     color: rgb(0, 238, 255);
     text-align: center;
