35de72a385371afdea1d6be34cbbe2129dc68687
MantaYi <318797061@qq.com>
2020-03-24 11:19:06 +0800

    数据库连接

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,12 +1,13 @@
 //引入mongoose
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 //使用mongoose
 mongoose.connect('mongodb://localhost/1902', {
-  useNewUrlParser: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 //连接数据库
-var db = mongoose.connection //数据库的连接对象
+let db = mongoose.connection; //数据库的连接对象
 db.on('error', console.error.bind(console, 'connection error:'))
 db.on('open', function () {
   console.log('数据库连接成功')
-})
+});
